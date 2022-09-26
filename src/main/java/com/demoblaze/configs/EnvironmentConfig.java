f663d10ab4cb3fbe2f165378d7d2b4ed44a4e769
k-yudin <telesens.yudin@gmail.com>
2022-09-26 12:53:09 +0300

    updated configuration settings | local - cloud run support

--- src/main/java/com/demoblaze/configs/EnvironmentConfig.java
+++ src/main/java/com/demoblaze/configs/EnvironmentConfig.java
@@ -8,6 +8,20 @@ public class EnvironmentConfig {
 
     public static String getBrowser() {
         String browser = properties.getProperty("BROWSER");
-        return browser;
+        if (browser != null && !browser.contains("$")) {
+            return browser;
+        }
+        else
+            return System.getenv("BROWSER");
     }
+
+    public static String getHost() {
+        String host = System.getenv("HOST");
+        if (host == null)
+            return "LOCAL";
+        else
+            return host;
+    }
+
+    public static String getRemoteURL() { return System.getenv("REMOTE_URL"); }
 }
